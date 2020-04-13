86921e7e3c5341f7fd7032e1b19c0240ba7b0289
creavin <thomas.creavin@gmail.com>
2020-04-13 20:34:27 +0100

    Image to video script

--- setupOS.sh
+++ setupOS.sh
@@ -3,8 +3,8 @@
 # Work in progress
 
 echo  "Setting up a new Arch xfce environment"
-pacman -Sy yay
-yay -S gvim ranger
+sudo pacman -Sy yay
+yay -S gvim ranger ristretto evince redshift
 yay -S spotify sncli transmission-gtk
 
 git config --global user.name creavin
